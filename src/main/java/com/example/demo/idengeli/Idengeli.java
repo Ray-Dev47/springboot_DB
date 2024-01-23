6d9d17ac6afe58ffb561e2b512e7b02aa7e6b7c4
Raymond <raymond.idengeli@gmail.com>
2024-01-23 23:26:42 +0100

    JPA conectivity and psql shell configuration

--- src/main/java/com/example/demo/idengeli/Idengeli.java
+++ src/main/java/com/example/demo/idengeli/Idengeli.java
@@ -1,5 +1,14 @@
 package com.example.demo.idengeli;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.time.LocalDate;
+
+
+
+@Entity
+@Table
+
 
 //definition of class data
 public class Idengeli {
